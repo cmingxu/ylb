8d348a09e1c0733106d468fddf863429f17a2263
Kevin.Xu <cmxu@kabam.com>
2013-12-27 14:00:49 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ylb::Application.config.secret_key_base = 'b8a439d2da9ed21c4ca31e2eb5558c078256eed9972d6d8bbf06a70ead5993acda10e78742c3ca4530d35ce7d15f4278c3c190a2647e5991236d3ae05a5dd610'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ylb::Application.config.session_store :cookie_store, key: '_ylb_session'
